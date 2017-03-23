5b6c74f7102123f109e640a3704fe55622aae896
Cess <cessmbuguar@gmail.com>
2017-03-23 10:13:33 +0300

    refactoring

--- atm_challenge/lib/atm.rb
+++ atm_challenge/lib/atm.rb
@@ -5,12 +5,20 @@ class Atm
   end
   def withdraw(amount, account)
     case 
-    when amount > account.balance
+    when insufficient_funds(amount, account)
       return
     else
+      transaction(amount, account)
+    end
+  end
+
+  private
+  def insufficient_funds(amount, account)
+    amount > account.balance
+  end
+  def transaction(amount, account)
     @funds -= amount
     account.balance = account.balance - amount
     {status: true, message: "Success!!", date: Date.today, amount: amount}
-    end
   end
 end
