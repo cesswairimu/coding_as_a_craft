563918b9e823521a11e70a864fc529788121bb78
Cess <cessmbuguar@gmail.com>
2017-03-24 11:36:53 +0300

    cases for atm withdrawal

--- atm_challenge/lib/atm.rb
+++ atm_challenge/lib/atm.rb
@@ -3,22 +3,44 @@ class Atm
   def initialize
     @funds = 1000
   end
-  def withdraw(amount, account)
+  def withdraw(amount, pin, account)
     case 
-    when insufficient_funds(amount, account)
-      return
+    when insufficient_funds_account(amount, account)
+    { status: false, message: 'You have insufficient funds!!', date: Date.today }
+    when insufficient_funds_atm(amount, account)
+    { status: false, message: 'Insufficient funds in ATM', date: Date.today }
+    when incorrect_pin(pin, account.pin)
+    { status: false, message: 'Incorrect Pin', date: Date.today }
+    when card_expired(account.exp_date)
+      { status: false, message: 'ATM Card Expired!!', date: Date.today }
+    when account.account_status == :disabled
+      { status: false, message: 'Your account is disabled!!', date: Date.today }
     else
       transaction(amount, account)
     end
   end
 
   private
-  def insufficient_funds(amount, account)
+  def insufficient_funds_account(amount, account)
     amount > account.balance
+
   end
+
   def transaction(amount, account)
     @funds -= amount
     account.balance = account.balance - amount
     {status: true, message: "Success!!", date: Date.today, amount: amount}
+
+
+  end
+  def insufficient_funds_atm(amount, account)
+    @funds < amount
+  end
+  def incorrect_pin(pin, actual_pin)
+    pin != actual_pin
+  end
+  def card_expired(exp_date)
+    Date.strptime(exp_date,'%m/%y')<Date.today
   end
 end
+
--- atm_challenge/spec/atm_spec.rb
+++ atm_challenge/spec/atm_spec.rb
@@ -1,6 +1,6 @@
 require './lib/atm.rb'
 describe Atm do
-  let(:account) { instance_double('Account') }
+  let(:account) { instance_double('Account', pin: 1234, exp_date: '04/17', account_status: :active) }
   before do
     #add atrribute of balance and set it to 100
     allow(account).to receive(:balance).and_return(100)
@@ -13,12 +13,34 @@ describe Atm do
   end
 
   it 'funds are reduced at a withdrawal' do
-    subject.withdraw(50, account)
+    subject.withdraw(50,1234, account)
     expect(subject.funds).to eq 950
   end
   it 'allows withdrawal if bank has enough funds' do
     expected_output ={ status: true, message: 'Success!!', date: Date.today, amount: 45 }
-    expect(subject.withdraw(45, account)).to eq(expected_output)
+    expect(subject.withdraw(45,1234, account)).to eq(expected_output)
+  end
+
+  it "rejects withdrawal if bank has insufficient funds" do
+    expected_output = { status: false, message: 'You have insufficient funds!!', date: Date.today }
+    expect(subject.withdraw(105, 1234, account)).to eq(expected_output)
+  end
+
+  it "rejects withdrawal if ATM has insufficient funds" do
+    subject.funds = 50
+    expected_output = { status: false, message: 'Insufficient funds in ATM', date: Date.today }
+    expect(subject.withdraw(100, 1234, account)).to eq(expected_output)
+end
+  it 'rejects withdrawal if Atm is expired' do
+    allow(account).to receive(:exp_date).and_return('12/15')
+    expected_output = { status: false, message: 'ATM Card Expired!!', date: Date.today }
+    expect(subject.withdraw(6, 1234, account)).to eq(expected_output)
+  end
+  it 'rejects withdrawal if account is disabled' do
+    allow(account).to receive(:account_status).and_return(:disabled)
+    expected_output = { status: false, message: 'Your account is disabled!!', date: Date.today }
+    expect(subject.withdraw(5, 1234, account)).to eq(expected_output)
+
   end
 end
 
