a4df2dd61351e985a06f34b90e02d00cd366facf
Cess <cessmbuguar@gmail.com>
2017-04-12 06:46:17 +0300

    yaml for book store and library class

--- library-challenge/lib/library.rb
+++ library-challenge/lib/library.rb
@@ -1,4 +1,9 @@
+FILENAME = './lib/books.yaml'
 class Library
-
+  attr_accessor :title, :due_date, :author
 
 end
+require 'yaml'
+u = YAML::load(File.open(FILENAME))
+puts u.first
+
--- library-challenge/spec/library_spec.rb
+++ library-challenge/spec/library_spec.rb
@@ -1,4 +1,7 @@
 require_relative '../lib/library.rb'
 describe Library do
 
+  it 'checks there is a collection of books' do
+    expect(:book).not_to be_empty
+  end
 end
